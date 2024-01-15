556b3b736624531fd6ddcc58be4ea22d753e9b28
lurwandanmusa <abubakarlurwandms@gmail.com>
2024-01-15 12:23:36 -0800

    first commit

---
+++ data.py
@@ -0,0 +1,28 @@
+# my_money = 700
+# if (my_money < 500):
+#     print('Buy awara to me')
+# elif(my_money == 500):
+#     print('Buy a rice to me')
+
+
+# else:
+#     print('Buy shawarma to me')
+
+math1301 = 56*3
+math1311 = 41*3
+phy1211 = 60*2
+Gsp1212 = 17*2
+chm1311 = 47*3
+
+gpa =(math1301 + math1311 + phy1211 + Gsp1212 +chm1311)/13
+print(gpa)
+math1301 =56
+if(math1301 <39):
+    print('fail')
+elif(math1301 >39 and math1301 <44):
+    print('E8')
+elif(math1301 >44 and math1301 <55):
+    print('D7')
+
+elif(math1301 >55 and math1301 <60):
+    print('pass')
